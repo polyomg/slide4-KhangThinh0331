7112d0cc576f6f90daa317fbb27080c2ea70e11b
KhangThinh0331 <khangthinh0331@gmail.com>
2025-10-02 13:38:48 +0700

    TC00224 - Le Khang Thinh - Slide 4 - First commit

---
+++ TC00224_L3_Slide4/src/main/java/com/poly/Tc00224L3Slide4Application.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tc00224L3Slide4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tc00224L3Slide4Application.class, args);
+	}
+
+}
---
+++ TC00224_L3_Slide4/src/test/java/com/poly/Tc00224L3Slide4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tc00224L3Slide4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
